6a93ce5330772ee033f6d7cd15c8039a8db9c049
Dorukhan Demir <79447326+DemirDorukhan@users.noreply.github.com>
2024-04-09 12:50:13 +0300

    Added MVC Design Pattern

---
+++ FunFactApp/Model/QuestionBox.swift
@@ -0,0 +1,45 @@
+//
+//  QuestionBox.swift
+//  FunFactApp
+//
+//  Created by Dorukhan Demir on 9.04.2024.
+//
+
+import Foundation
+
+
+struct QuestionBox{
+    
+    let questions = [
+        Question(q: "2 + 2 = 5", a: "False"),
+        Question(q: "3 + 4 = 7", a: "True"),
+        Question(q: "5 + 4 = 9", a: "True")
+    ]
+    
+    var question_number = 0
+    
+    func checkAnswer(_ userAnswer : String) -> Bool {
+        if userAnswer == questions[question_number].answer{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func getText() -> String {
+        return questions[question_number].text
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float(question_number) / Float(questions.count)
+        return progress
+    }
+    
+    mutating func nextQuestion() {
+        if question_number + 1 < questions.count {
+            question_number += 1
+        }else{
+            question_number = 0
+        }
+    }
+}
